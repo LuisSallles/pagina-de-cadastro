454e41d41d4a13ba9392b3677f6d93f20a283c04
Luis Salles <107654852+LuisSallles@users.noreply.github.com>
2023-08-25 20:28:56 -0300

    .

--- javascript.js
+++ javascript.js
@@ -1,12 +1,16 @@
 function check() {
 
-var name = Number(window.document.getElementById('txtname').value)
-var email = String(window.document.getElementById('txtemail').value)
-var pass = Number(window.document.getElementById('txtpass').value)
-var pass2 = Number(window.document.getElementById('txtpass2').value)
-var res = window.document.getElementById('res')
+    var name = Number(window.document.getElementById('txtname').value)
+    var email = String(window.document.getElementById('txtemail').value)
+    var pass = Number(window.document.getElementById('txtpass').value)
+    var pass2 = Number(window.document.getElementById('txtpass2').value)
+    var res = window.document.getElementById('res')
 
-if (name === 0 || email === 0 || pass == 0 || pass2 == 0) {
-    res.innerHTML = `<p>Please, fill in the details above!</p>`
-}
+    if (name === 0 || email === 0 || pass == 0 || pass2 == 0) {
+        res.innerHTML = `<p><strong>Please, fill in the details above!</strong></p>`
+        res.style.color = 'red'
+    } else {
+        res.innerHTML = `<p><strong>Very Good! Come back tomorrow!!</strong></p>`
+        res.style.color = 'green'
+    }
 }
