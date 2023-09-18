abf984b0f5528de8d1e2e19a8acedb38bc80c5c3
Luis Salles <107654852+LuisSallles@users.noreply.github.com>
2023-09-18 15:01:04 -0300

    18/09/2023

--- javascript.js
+++ javascript.js
@@ -1,16 +1,18 @@
-function check() {
-
-    var name = Number(window.document.getElementById('txtname').value)
-    var email = String(window.document.getElementById('txtemail').value)
-    var pass = Number(window.document.getElementById('txtpass').value)
-    var pass2 = Number(window.document.getElementById('txtpass2').value)
-    var res = window.document.getElementById('res')
-
-    if (name === 0 || email === 0 || pass == 0 || pass2 == 0) {
+function checkData(n, e, p, p2) { // Here will check if the user fill the details
+    const res = window.document.getElementById('res')
+    if (n === 0 || e === 0 || p == 0 || p == 0) {
         res.innerHTML = `<p><strong>Please, fill in the details above!</strong></p>`
         res.style.color = 'red'
     } else {
         res.innerHTML = `<p><strong>Very Good! Come back tomorrow!!</strong></p>`
         res.style.color = 'green'
     }
+}
+
+function check() { // Function to check if the user fill in the data
+    let name = Number(window.document.getElementById('txtname').value) 
+    const email = String(window.document.getElementById('txtemail').value)
+    const pass = Number(window.document.getElementById('txtpass').value)
+    const pass2 = Number(window.document.getElementById('txtpass2').value)
+    checkData(name, email, pass, pass2, res)
 }
